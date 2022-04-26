2578c562786e18776da44a430d2bb06846a6e749
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2022-04-26 20:21:01 +0300

    Patch1

---
+++ lab6try1/Points.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab6try1
+{
+    public class Point
+    {
+        private float latitude; // x
+
+        private float length; // y
+
+        private int radius;
+
+        private string name;
+        private string type;
+
+        /*
+         for each point in points
+            if in radius 
+                in_radius = true
+                display
+            
+         
+         Math.Sqrt((X - point.X) * (X - point.X) + (Y - point.Y) * (Y - point.Y))
+         */
+        
+        public double DistanceTo(Point another)
+        {
+            double result = 0;
+            result = Math.Sqrt((this.latitude - another.latitude)*(this.latitude - another.latitude)+(this.length - another.length)*(this.length - another.length));
+            return result;
+        }
+        
+        
+    }
+   
+    
+}
+
+
+
+
+
+
+
